2c31f91c07d6ba4f45953105546515bcad943116
Antonio Paternina <acpat88@gmail.com>
2016-09-30 22:13:31 -0500

    3 - Montar un web server simple Se refactoriza el código para que sea más modular.

--- index.js
+++ index.js
@@ -4,24 +4,36 @@ var http = require('http');
 var server = http.createServer(function (request, response) {
     console.log('Recibí un request ' + request.url);
 
+    function serveStaticFile(fileLocation, callback) {
+        fs.readFile(fileLocation, null, function (err, data) {
+            if (err) {
+                callback(err);
+            }
+
+            callback(err, data.toString());
+        });
+    }
+
+    function writeResponse(response, data) {
+        response.writeHead(200, {'Content-Type': data.contentType});
+        response.end(data.data);
+    }
+
     switch (request.url) {
         case '/':
-            response.writeHead(200, {'Content-Type': 'text/html'});
-            fs.readFile('./public/index.html', function (err, data) {
-                response.end(data);
-            })
+            serveStaticFile('./public/index.html', function (err, content) {
+                writeResponse(response, {contentType: 'text/html', data: content})
+            });
             break;
         case '/app.js':
-            response.writeHead(200, {'Content-Type': 'application/javascript'});
-            fs.readFile('./public/app.js', function (err, data) {
-                response.end(data);
-            })
+            serveStaticFile('./public/app.js', function (err, content) {
+                writeResponse(response, {contentType: 'text/html', data: content})
+            });
             break;
         case '/app.css':
-            response.writeHead(200, {'Content-Type': 'text/css'});
-            fs.readFile('./public/app.css', function (err, data) {
-                response.end(data);
-            })
+            serveStaticFile('./public/app.css', function (err, content) {
+                writeResponse(response, {contentType: 'text/css', data: content})
+            });
             break;
         default:
             response.end('');
